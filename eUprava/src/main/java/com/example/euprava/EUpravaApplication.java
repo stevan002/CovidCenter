5edb19f6eb417a14b35eda23892d54f8902a18eb
stevan002 <stevanstankovic603@gmail.com>
2023-01-13 17:29:11 +0100

    Prvi commit

---
+++ eUprava/src/main/java/com/example/euprava/EUpravaApplication.java
@@ -0,0 +1,13 @@
+package com.example.euprava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EUpravaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EUpravaApplication.class, args);
+    }
+
+}
---
+++ eUprava/src/test/java/com/example/euprava/EUpravaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.euprava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EUpravaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
